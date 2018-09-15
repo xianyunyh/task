2adce942a4d156798a7c3ce62df52e21f19c127c
xianyunyh <tianlei542475736@gmail.com>
2018-09-15 22:51:31 +0800

    增加消息处理handler

--- src/Job.php
+++ src/Job.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace Task;
 
 use Swoole\Table;
+
 class Job
 {
     /**
@@ -9,21 +11,22 @@ class Job
      */
     public static $table;
 
-    public static function init($size = 1024,$columns = [] )
+    public static function init($size = 1024, $columns = [])
     {
         $table = new Table($size);
         foreach ($columns as $column) {
-            $table->column($column[0],$column[1],$column[2]);
+            $table->column($column[0], $column[1], $column[2]);
         }
         $table->create();
         self::$table = $table;
         return new self();
     }
 
-    public static function add($key,array $data)
+    public static function add($key, array $data)
     {
-        return self::$table->set($key,$data);
+        return self::$table->set($key, $data);
     }
+
     public static function get($key)
     {
         return self::$table->get($key);
@@ -34,16 +37,29 @@ class Job
         return self::$table->del($key);
     }
 
-    public static function update($key,array $data)
+    public static function update($key, array $data)
     {
-        if(self::$table->exist($key)) {
-            return self::$table->set($key,$data);
+        if (self::$table->exist($key)) {
+            return self::$table->set($key, $data);
         }
         return false;
     }
-    public static function count ()
+
+    public static function count()
     {
         return self::$table->count();
+    }
+
+    public static function all()
+    {
+        $rows = [];
+        if (self::count() <= 0) {
+            return $rows;
+        }
+        foreach (self::$table as $row) {
+            $rows[] = $row;
+        }
+        return $rows;
     }
 
 
--- src/Tools/Message.php
+++ src/Tools/Message.php
@@ -1,20 +1,54 @@
 <?php
 namespace Task\Tools;
+use Task\Job;
+use Task\Singleton;
 
 class Message
 {
+    use Singleton;
+
+    protected $typeList = ['add','update','delete','select','ping'];
     /**
      * 解开消息
      * @param $message
      * @return bool|array
      */
-    public static function decode($message)
+    public function decode($message)
     {
         if(empty($message)) {
             return false;
         }
         //now use json_decode
         return json_decode($message,true);
+    }
+
+    public function handler($message, Job $jobInstance)
+    {
+        $message = $this->decode($message);
+        if(false === $message) {
+            return false;
+        }
+        $type = $message['type'];
+        $data = $message['data'];
+        switch ($type){
+            case 'add':
+                $jobInstance::add($data['task_id'],$data);
+                break;
+            case 'delete':
+                $jobInstance::remove($data['task_id']);
+                break;
+            case 'update':
+                $jobInstance::update($data['task_id'],$data);
+                break;
+            case 'select':
+                break;
+            case 'ping':
+                return 'pong';
+                break;
+            default:
+                return [];
+                break;
+        }
     }
 
 }
